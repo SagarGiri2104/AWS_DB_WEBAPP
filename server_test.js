ffe948bec7e5a1ec2b48518d547a1c32cc20db7a
Samar <sagargearsup@gmail.com>
2020-10-01 20:37:19 -0500

    Update and rename server1.js to server_test.js

--- server_test.js
+++ server_test.js
@@ -3,30 +3,22 @@ var fs = require("fs");
 var path = require('path');
 const express = require("express");
 const bodyParser = require("body-parser");
-//const handleBars = require("express-handlebars");
+const handleBars = require("express-handlebars");
 const mysqlConnection = require("./connection");
-const db2connect = require("./db2Connection");
-
-//above are all import statements
 
 var peopleRoutes = require("./routes/people");
 var index1 = require("./routes/index");
-//const index2 = require("./routes/index");
-//const index3 = require("./routes/editData");
 
 //create express app
 var app = express();
 var options = {
     index: 'index.html'
 };
-//app.use('/', express.static('/home/site/wwwroot', options));
+
 app.use(bodyParser.json());
 app.use("/", index1);
 app.use("/people", peopleRoutes);
-//app.use("/people", peopleRoutes);
 app.use("/project", index1);
-//app.use("/editdata", index2);
-//app.use("/us3", index3);
 
 app.set('view engine', 'html');
 app.set('views', path.join(__dirname, 'views'));
